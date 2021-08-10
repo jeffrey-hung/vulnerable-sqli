b7640cd413e1658ff782a71e1731eba6f0ec0b9e
jeffrey-hung <jeffreyhung@live.com>
2021-08-10 12:59:19 +0800

    fixed index.php

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
-
-If Nessus does not run ->
-
-
-MBSA approach
-------------
-Remote Registry = on
-Netbios
-SMB
-------------
-
-
-Windows update - missing update
-------------
-View what year the machine has been updated to
-------------
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Search</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+	<form action="search.php" method="GET">
+		<input type="text" name="query" />
+		<input type="submit" value="Search" />
+	</form>
+</body>
+</html>
