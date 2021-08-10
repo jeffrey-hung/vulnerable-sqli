98d9c9c886e470ae49d795b6974c13f2464ac203
jeffrey-hung <jeffreyhung@live.com>
2021-08-10 12:54:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+
+If Nessus does not run ->
+
+
+MBSA approach
+------------
+Remote Registry = on
+Netbios
+SMB
+------------
+
+
+Windows update - missing update
+------------
+View what year the machine has been updated to
+------------
