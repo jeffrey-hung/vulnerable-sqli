cf537538444351ba04323bd90669ae9c2283de16
jeffrey-hung <jeffreyhung@live.com>
2021-08-10 14:08:48 +0800

    fdsfds

--- search.php
+++ search.php
@@ -1,3 +1,15 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "root");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$db_selected = mysqli_select_db($link, "test_data");
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
