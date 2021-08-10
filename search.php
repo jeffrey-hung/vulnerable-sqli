59c93232ec191619cbec14c0c15cbd5f59090db0
jeffrey-hung <jeffreyhung@live.com>
2021-08-10 14:16:12 +0800

    gfdg

--- search.php
+++ search.php
@@ -9,17 +9,11 @@ if($link === false){
 }
 
 $db_selected = mysqli_select_db($link, "test_data");
-?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<title>Search results</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" type="text/css" href="style.css"/>
-</head>
-<body>
-<?php
+if (!$db_selected) {
+    die('Cannot access' . DB_NAME . ': ' . mysqli_error());
+}
+
 	$query = $_GET['query']; 
 	// gets value sent over search form
 	$sql= "SELECT * FROM test_data WHERE `username` LIKE '%" . $query . "%'"
