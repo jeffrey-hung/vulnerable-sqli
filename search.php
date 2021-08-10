715cef2843737e957c735cdb3fd804b93ee56421
jeffrey-hung <jeffreyhung@live.com>
2021-08-10 14:17:28 +0800

    fsafa

--- search.php
+++ search.php
@@ -1,50 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Search results</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "root");
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-$db_selected = mysqli_select_db($link, "test_data");
-
-if (!$db_selected) {
-    die('Cannot access' . DB_NAME . ': ' . mysqli_error());
-}
-
-	$query = $_GET['query']; 
-	// gets value sent over search form
-	$sql= "SELECT * FROM test_data WHERE `username` LIKE '%" . $query . "%'"
-		
-	$raw_results = mysqli_query($link, $sql);
-			
-		// * means that it selects all fields, you can also write: `id`, `title`, `text`
-		// articles is the name of our table
-		
-		// '%$query%' is what we're looking for, % means anything, for example if $query is Hello
-		// it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
-		// or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
-		
-		if(mysql_num_rows($raw_results) > 0){ // if one or more rows are returned do following
-			
-			while($results = mysql_fetch_array($raw_results)){
-			// $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-			
-				echo "<p><h3>".$results['username']."</h3></p>";
-				// posts results gotten from database(title and text) you can also show id ($results['id'])
-			}
-			
-		}
-		else{ // if there is no matching rows do following
-			echo "No results";
-		}
-		
+	/* Attempt MySQL server connection. Assuming you are running MySQL
+	server with default setting (user 'root' with no password) */
+	$link = mysqli_connect("localhost", "root", "root");
+	 
+	// Check connection
+	if($link === false){
+		die("ERROR: Could not connect. " . mysqli_connect_error());
 	}
-	else{ // if query length is less than minimum
-		echo "Minimum length is ".$min_length;
+
+	$db_selected = mysqli_select_db($link, "test_data");
+
+	if (!$db_selected) {
+		die('Cannot access' . DB_NAME . ': ' . mysqli_error());
 	}
+	
 ?>
 </body>
 </html>
