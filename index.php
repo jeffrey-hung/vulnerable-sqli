9cafccd58fa0d2c09556356baf4cd6ba0415c864
jeffrey-hung <jeffreyhung@live.com>
2021-08-17 10:08:40 +0800

    treqt

--- index.php
+++ index.php
@@ -40,9 +40,9 @@ SEARCH USERS
 			while($row = mysqli_fetch_assoc($result)) {
 				echo "username: " . $row["username"]. " - Password: " . $row["password"];
 				echo '<table>
-				   <tr><th>Username</th></tr>
-				   <tr><td>'.$row["username"].'</td></tr>
-				    <tr><th>Password</th></tr>
+				   <tr><th>Username</th>
+				   <th>Password</th></tr>
+				   <tr><td>'.$row["username"].'</td></tr> 
 				   <tr><td>'.$row["password"].'</td></tr>
 				</table>';
 			}
